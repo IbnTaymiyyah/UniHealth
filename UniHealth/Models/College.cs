f09f337c6b196b4f7c24e94153209822eb04fbe0
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-03 18:45:12 +0200

    Add : College Model Class

---
+++ UniHealth/Models/College.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.Models
+{
+    public class College
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
