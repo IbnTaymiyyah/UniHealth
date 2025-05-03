5262b30e817ea3d309c81dec87c9ffafb208d17e
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-03 16:49:27 +0200

    Add : User Model

---
+++ UniHealth/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LName { get; set; }
+
+        [MaxLength(100)]
+        public string phoneNumber { get; set; }
+
+        [MaxLength(100)]
+        public string imageUrl { get; set; }
+        
+        [MaxLength(100)]
+        public string userName { get; set; }
+
+        [MaxLength(100)]
+        public string password { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
