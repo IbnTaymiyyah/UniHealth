b990a13532c7ab93221f716fb65f3bcb3787df1d
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-03 21:37:43 +0200

    Add : UserDTO Class

---
+++ UniHealth/DTO/CreateUserDTO.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class CreateUserDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LName { get; set; }
+       
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
+
+        
+        public string ImageUrl { get; set; }
+    }
+}
