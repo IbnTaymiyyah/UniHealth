7dc5f5fdbd34baaf8922d543f7234d70636a8e3f
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-03 19:37:07 +0200

    Add : StudentDetail Model Class

---
+++ UniHealth/Models/StudentDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.Models
+{
+    public class StudentDetail
+    {
+        public int Id { get; set; }
+
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        
+        [ForeignKey("Univeristy")]
+        public int UniveristyId { get; set; }
+        public Univeristy Univeristy { get; set; }
+        
+        [ForeignKey("College")]
+        public int CollegeId { get; set; }
+        public College College { get; set; }
+    }
+}
