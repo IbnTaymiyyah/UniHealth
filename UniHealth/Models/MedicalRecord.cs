db1c2f048f7302b5fa6b848dacdb8ebdb29281bc
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-03 21:25:07 +0200

    Add : MedicalRecord Model Class

---
+++ UniHealth/Models/MedicalRecord.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.Models
+{
+    public class MedicalRecord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public int hasSickness { get; set; }
+
+        
+        [MaxLength(100)]
+        public DateTime? createdAt { get; set; }
+
+
+        public string imageUrl { get; set; }
+
+       
+
+        
+    }
+}
