f7dd3bcf4cbafdf858a57496f48c7774ffbfc154
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-03 21:26:19 +0200

    Add : Doctor Model Class

---
+++ UniHealth/Models/Doctor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.Models
+{
+    public class Doctor
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        
+        [ForeignKey("Univeristy")]
+        public int UniveristyId { get; set; }
+        public Univeristy Univeristy { get; set; }
+    }
+}
