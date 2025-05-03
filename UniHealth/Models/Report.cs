4b03b78d28f1955f3026bbbadcefb0a1fc96ab9d
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-03 21:25:49 +0200

    Add : Report Model Class

---
+++ UniHealth/Models/Report.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.Models
+{
+    public class Report
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        [MaxLength(2000)]
+        public string Notes { get; set; }
+
+        [ForeignKey("Doctor")]
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+    }
+}
