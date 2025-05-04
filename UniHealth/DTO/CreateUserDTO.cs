38d168b7fe197e4f040e7827f8d2f42308b43979
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 18:27:39 +0200

    Update : Validation In DTO Classes

--- UniHealth/DTO/CreateUserDTO.cs
+++ UniHealth/DTO/CreateUserDTO.cs
@@ -4,27 +4,28 @@ namespace UniHealth.DTO
 {
     public class CreateUserDTO
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = " مطلوب")]
+        [MaxLength(100, ErrorMessage = "يجب ألا يتجاوز الاسم 100 حرف")]
         public string FName { get; set; }
 
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = " مطلوب")]
+        [MaxLength(100, ErrorMessage = "يجب ألا يتجاوز الاسم 100 حرف")]
         public string LName { get; set; }
-       
 
-        [Required]
+
+        [Required(ErrorMessage = " مطلوب")]
+        [MaxLength(100, ErrorMessage = "يجب ألا يتجاوز الاسم 100 حرف")]
         public string PhoneNumber { get; set; }
 
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = " مطلوب")]
+        [Phone(ErrorMessage = "رقم الهاتف غير صحيح")]
         public string Username { get; set; }
 
 
-        [Required]
+        [Required(ErrorMessage = " مطلوب")]
         public int RoleId { get; set; }
 
-        
+      
         public string ImageUrl { get; set; }
     }
 }
