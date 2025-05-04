01eac09776cf9a96480f343566737006bd491461
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 11:58:41 +0200

    Add : UpdateMedicalRecordDto CLass

---
+++ UniHealth/DTO/UpdateMedicalRecordDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class UpdateMedicalRecordDto
+    {
+        [Required]
+        public int HasSickness { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
