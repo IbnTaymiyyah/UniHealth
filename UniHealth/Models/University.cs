8e875dae17d6e72cffccbcfe3b6e47dd2fc57b3d
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 19:57:33 +0200

    Fix : Class Name

---
+++ UniHealth/Models/University.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.Models
+{
+    public class University
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        
+    }
+}
