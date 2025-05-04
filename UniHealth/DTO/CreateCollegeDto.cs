fc46a6575542b98d20e51d0465554c918f08d29b
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 12:43:42 +0200

    Add : CreateCollegeDto Class

---
+++ UniHealth/DTO/CreateCollegeDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class CreateCollegeDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
