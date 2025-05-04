dcb557969ba62514c9dfd61b164e751bff7daa27
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 12:57:02 +0200

    Add : CreateReportDto

---
+++ UniHealth/DTO/CreateReportDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class CreateReportDto
+    {
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        [MaxLength(2000)]
+        public string Notes { get; set; }
+    }
+}
