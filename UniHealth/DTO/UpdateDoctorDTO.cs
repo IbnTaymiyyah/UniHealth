944e2798efbbe76b5d4e6c19b634a07a7a0b9d59
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 13:11:29 +0200

    Add : UpdateDoctorDTO

---
+++ UniHealth/DTO/UpdateDoctorDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class UpdateDoctorDTO
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int UniversityId { get; set; }
+    }
+}
