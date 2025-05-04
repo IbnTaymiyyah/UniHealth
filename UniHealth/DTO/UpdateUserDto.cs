6a8c60fca4e48f5b47f170414c2538fd0408df5a
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 12:04:00 +0200

    Add : Update User DTO Class

---
+++ UniHealth/DTO/UpdateUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class UpdateUserDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; }
+
+
+        public string ImageUrl { get; set; }
+    }
+}
