7b70026cab8d52a9682cc621f4a9546c96e1f40e
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 18:25:11 +0200

    Update : Validation In DTO Classes

--- UniHealth/DTO/ChangeUsernameDto.cs
+++ UniHealth/DTO/ChangeUsernameDto.cs
@@ -4,10 +4,11 @@ namespace UniHealth.DTO
 {
     public class ChangeUsernameDto
     {
-        [Required]
+        [Required(ErrorMessage = " مطلوب")]
+        [RegularExpression(@"^[a-zA-Zء-ي\s]+$", ErrorMessage = "يجب أن يحتوي الاسم على أحرف فقط")]
         public string NewUsername { get; set; }
 
-        [Required, DataType(DataType.Password)]
+        [Required(ErrorMessage = " مطلوب"), DataType(DataType.Password)]
         public string CurrentPassword { get; set; }
     }
 }
