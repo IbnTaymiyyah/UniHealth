e1ad2fd8bfd1fad9ed123cd2f45f88c4020f0cf2
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 11:58:25 +0200

    Add : CreateMedicalRecordDto Class

---
+++ UniHealth/DTO/CreateMedicalRecordDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class CreateMedicalRecordDto
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int HasSickness { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
