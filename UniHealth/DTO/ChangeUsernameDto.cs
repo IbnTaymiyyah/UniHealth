b4ce5211f0c0317439a9053b5591b2123b40c16a
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 11:11:08 +0200

    Add : ChangeUsernameDto Class

---
+++ UniHealth/DTO/ChangeUsernameDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class ChangeUsernameDto
+    {
+        [Required]
+        public string NewUsername { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+    }
+}
