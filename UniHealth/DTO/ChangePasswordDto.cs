4a9c90afb05d4729081aa7a8ca39dde8e3876be1
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 11:35:38 +0200

    Add : ChangePasswordDto Class

---
+++ UniHealth/DTO/ChangePasswordDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class ChangePasswordDto
+    {
+        [Required , DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+        
+        [Required , DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword")]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
