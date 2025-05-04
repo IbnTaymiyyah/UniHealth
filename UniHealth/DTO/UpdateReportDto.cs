9ad7187394c0679f35f12afcb34753a4bda66ca1
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-04 12:56:48 +0200

    Add : UpdateReportDto

---
+++ UniHealth/DTO/UpdateReportDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO
+{
+    public class UpdateReportDto
+    {
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        [MaxLength(2000)]
+        public string Notes { get; set; }
+    }
+}
