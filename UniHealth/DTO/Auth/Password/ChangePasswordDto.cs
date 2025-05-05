d86a1e5839d5ddf8c82092545e7ae91c79e0a864
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 19:11:12 +0200

    Add : Change Password DTO

---
+++ UniHealth/DTO/Auth/Password/ChangePasswordDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Auth.Password
+{
+    public class ChangePasswordDto
+    {
+        
+        [Required(ErrorMessage = " مطلوب"), DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+        
+        [Required(ErrorMessage = " مطلوب"), DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "كلمة المرور غير متطابقة")]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
