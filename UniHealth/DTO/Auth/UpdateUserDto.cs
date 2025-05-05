257f83cdd15a68fbe7855bdeb343e72a306a2d80
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 19:09:59 +0200

    Add : Update User DTO

---
+++ UniHealth/DTO/Auth/UpdateUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Auth
+{
+    public class UpdateUserDto
+    {
+        [Required(ErrorMessage = " مطلوب")]
+        [MaxLength(100, ErrorMessage = "يجب ألا يتجاوز الاسم 100 حرف")]
+        public string FName { get; set; }
+
+        [Required(ErrorMessage = " مطلوب")]
+        [MaxLength(100, ErrorMessage = "يجب ألا يتجاوز الاسم 100 حرف")]
+        public string LName { get; set; }
+
+        [Required(ErrorMessage = " مطلوب")]
+        [Phone(ErrorMessage = "رقم الهاتف غير صحيح")]
+        public string PhoneNumber { get; set; }
+
+
+        public string ImageUrl { get; set; }
+    }
+}
