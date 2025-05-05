4e6bed502246e9694e8a1b886cc313e16e7c450b
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 19:11:49 +0200

    Add : Verfy Password DTO

---
+++ UniHealth/DTO/Auth/Password/VerifyResetPasswordDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Auth.Password
+{
+    public class VerifyResetPasswordDTO
+    {
+        [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+        [EmailAddress(ErrorMessage = "البريد الإلكتروني غير صحيح")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "كود التحقق مطلوب")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "كود التحقق يجب أن يكون 6 أرقام")]
+        public string VerificationCode { get; set; }
+    }
+}
