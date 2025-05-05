a9916fa94546231e0487b56d15dfe0df7007ceff
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 19:13:16 +0200

    Add : Update Medical record DTO

---
+++ UniHealth/DTO/Medical/MedicalRecord/UpdateMedicalRecordDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Medical.MedicalRecord
+{
+    public class UpdateMedicalRecordDto
+    {
+        [Required(ErrorMessage = " مطلوب")]
+        public int HasSickness { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
