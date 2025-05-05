2e929aa6ae5f37819790c4febc3cd68cb5628fa2
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-06 00:42:05 +0200

    Add : Student Response DTO Class

---
+++ UniHealth/response/Models Response/StudentDetailResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace UniHealth.response.Models_Response
+{
+    public class StudentDetailResponseDto
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string UniversityName { get; set; }
+        public string CollegeName { get; set; }
+    }
+}
