279c348ede8a77e570e94272277049da6e1394e8
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-06 00:43:17 +0200

    Add : Medical Mapping Profile Class

---
+++ UniHealth/Mapping/MedicalMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using UniHealth.DTO.Medical.MedicalRecord;
+using UniHealth.DTO.Medical.Report;
+using UniHealth.Models;
+using UniHealth.response.Models_Response;
+
+namespace UniHealth.Mapping
+{
+    public class MedicalMappingProfile : Profile
+    {
+        public MedicalMappingProfile()
+        {
+            // MedicalRecord
+            CreateMap<CreateMedicalRecordDto, MedicalRecord>();
+            CreateMap<UpdateMedicalRecordDto, MedicalRecord>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            // Report
+            CreateMap<CreateReportDto, Report>();
+            CreateMap<UpdateReportDto, Report>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            // Report -> ReportResponseDto
+            CreateMap<Report, ReportResponseDto>()
+                .ForMember(dest => dest.StudentName, opt => opt.MapFrom(src => $"{src.User.FName} {src.User.LName}"))
+                .ForMember(dest => dest.DoctorName, opt => opt.MapFrom(src => $"{src.Doctor.User.FName} {src.Doctor.User.LName}"));
+       
+        }
+    }
+}
