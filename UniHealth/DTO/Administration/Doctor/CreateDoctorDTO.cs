4305490abd69f68000a11d7145333d1ad9369d66
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 18:29:39 +0200

    Add : Create Doctor DTO

---
+++ UniHealth/DTO/Administration/Doctor/CreateDoctorDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Administration.Doctor
+{
+    public class CreateDoctorDTO
+    {
+        [Required(ErrorMessage = " مطلوب")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = " مطلوب")]
+        public int UniversityId { get; set; }
+    }
+}
