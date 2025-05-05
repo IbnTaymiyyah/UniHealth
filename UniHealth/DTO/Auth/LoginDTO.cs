37befe24e33f4bac0c7fe1cb4746b98fa4580aae
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 19:10:22 +0200

    Add : Login DTO

---
+++ UniHealth/DTO/Auth/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Auth
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = " مطلوب")]
+        [RegularExpression(@"^[a-zA-Zء-ي\s]+$", ErrorMessage = "يجب أن يحتوي الاسم على أحرف فقط")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = " مطلوب"), DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+    }
+}
