e30886a9dc0cd91e18d949ac1953ab37e3d76f91
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 19:14:37 +0200

    Add : Update Report DTO

---
+++ UniHealth/DTO/Medical/Report/UpdateReportDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Medical.Report
+{
+    public class UpdateReportDto
+    {
+
+        [Required(ErrorMessage = " مطلوب")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = " مطلوب")]
+        [MaxLength(200, ErrorMessage = "يجب ألا يتجاوز الاسم 200 حرف")]
+        public string Title { get; set; }
+
+
+        [MaxLength(2000, ErrorMessage = "يجب ألا يتجاوز الاسم 2000 حرف")]
+        public string Description { get; set; }
+
+
+        [MaxLength(2000, ErrorMessage = "يجب ألا يتجاوز الاسم 2000 حرف")]
+        public string Notes { get; set; }
+    }
+}
