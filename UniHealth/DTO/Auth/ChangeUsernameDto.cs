9f9b09fa96dfec2dfaa65016292fb5bcf8913ed2
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 18:31:49 +0200

    Add : Change UserName DTO

---
+++ UniHealth/DTO/Auth/ChangeUsernameDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Auth
+{
+    public class ChangeUsernameDto
+    {
+        [Required(ErrorMessage = " مطلوب")]
+        [RegularExpression(@"^[a-zA-Zء-ي\s]+$", ErrorMessage = "يجب أن يحتوي الاسم على أحرف فقط")]
+        public string NewUsername { get; set; }
+
+        [Required(ErrorMessage = " مطلوب"), DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+    }
+}
