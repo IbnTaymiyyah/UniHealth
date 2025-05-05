37708922e27d45986a37c095b7ae720ff40cb198
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-06 00:41:00 +0200

    Add : Doctor Response DTO Class

---
+++ UniHealth/response/Models Response/DoctorResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UniHealth.response.Models_Response
+{
+    public class DoctorResponseDto
+    {
+        public int Id { get; set; }
+        public string UniversityName { get; set; }
+        public string DoctorName { get; set; }
+    }
+}
