d26d457612a1350017f12895140476d3372ea669
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 18:26:43 +0200

    Add : Update Student DTO

---
+++ UniHealth/DTO/Academic/StudentDetail/UpdateStudentDetailDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Academic.StudentDetail
+{
+    public class UpdateStudentDetailDto
+    {
+        [Required(ErrorMessage = " مطلوب")]
+        public int CollegeId { get; set; }
+
+        [Required(ErrorMessage = " مطلوب")]
+        public int UniversityId { get; set; }
+    }
+}
