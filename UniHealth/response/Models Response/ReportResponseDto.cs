b1da547f85e369a6a2e5c89a0b5d6c9ad03f2e7a
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-06 00:41:23 +0200

    Add : Report Response DTO Class

---
+++ UniHealth/response/Models Response/ReportResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UniHealth.response.Models_Response
+{
+    public class ReportResponseDto
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string DoctorName { get; set; } 
+    }
+}
