8013c24890b835012ade728126c9e9b424c1daaf
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 19:11:36 +0200

    Add : Forget Password DTO

---
+++ UniHealth/DTO/Auth/Password/ForgetPasswordRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Auth.Password
+{
+    public class ForgetPasswordRequestDTO
+    {
+        [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+        [EmailAddress(ErrorMessage = "البريد الإلكتروني غير صحيح")]
+        public string Email { get; set; }
+    }
+}
