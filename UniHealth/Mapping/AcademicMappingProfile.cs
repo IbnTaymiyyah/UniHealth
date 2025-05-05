7b3000bfbd600ac3aa619bb344389e618acff8ab
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-06 00:42:37 +0200

    Add : Acadmic Mapping Profile Class

---
+++ UniHealth/Mapping/AcademicMappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using UniHealth.DTO.Academic.StudentDetail;
+using UniHealth.DTO.Academic.University;
+using UniHealth.Models;
+using UniHealth.response.Models_Response;
+
+namespace UniHealth.Mapping
+{
+    public class AcademicMappingProfile : Profile
+    {
+        public AcademicMappingProfile()
+        {
+            // StudentDetail
+            CreateMap<CreateStudentDetailDto, StudentDetail>();
+            CreateMap<UpdateStudentDetailDto, StudentDetail>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            // University
+            CreateMap<CreateUniversityDto, University>();
+            CreateMap<UpdateUniversityDto, University>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+
+            // StudentDetail -> StudentDetailResponseDto
+            CreateMap<StudentDetail, StudentDetailResponseDto>()
+                .ForMember(dest => dest.StudentName, opt => opt.MapFrom(src => $"{src.User.FName} {src.User.LName}"))
+                .ForMember(dest => dest.UniversityName, opt => opt.MapFrom(src => src.Univeristy.Name))
+                .ForMember(dest => dest.CollegeName, opt => opt.MapFrom(src => src.College.Name));
+        }
+        }
+}
