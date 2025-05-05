ddb187ead03bc9c1292de701f56f9b47fe7af6c5
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 18:27:51 +0200

    Add : Create Univrsity DTO

---
+++ UniHealth/DTO/Academic/University/CreateUniversityDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Academic.University
+{
+    public class CreateUniversityDto
+    {
+        [Required(ErrorMessage = " مطلوب")]
+        [MaxLength(100, ErrorMessage = "يجب ألا يتجاوز الاسم 100 حرف")]
+        public string Name { get; set; }
+    }
+}
