8183327d525b50c06d2d835b1cb7ca6c09de2978
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-05 18:25:50 +0200

    Add : Create Student DTO

---
+++ UniHealth/DTO/Academic/StudentDetail/CreateStudentDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.DTO.Academic.StudentDetail
+{
+    public class CreateStudentDetailDto
+    {
+        [Required(ErrorMessage = " مطلوب")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = " مطلوب")]
+        public int CollegeId { get; set; }
+
+        [Required(ErrorMessage = " مطلوب")]
+        public int UniversityId { get; set; }
+    }
+}
