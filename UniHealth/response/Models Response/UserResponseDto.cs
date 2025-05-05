71e148efef8fa843deac787218b907ee352ea208
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-06 00:41:39 +0200

    Add : User Response DTO Class

---
+++ UniHealth/response/Models Response/UserResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace UniHealth.response.Models_Response
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string RoleName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
