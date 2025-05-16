ef3b090688100f67779c8461ba67bb622cacfc6d
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-16 16:03:08 +0200

    Add : Reset Password Class

---
+++ UniHealth/Models/PasswordReset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniHealth.Models
+{
+    public class PasswordReset
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
