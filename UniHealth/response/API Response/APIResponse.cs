eaf0095f324d8ec7de7ff2a1b995c37edf2f6cab
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-16 16:02:10 +0200

    Add : API Response

---
+++ UniHealth/response/API Response/APIResponse.cs
@@ -0,0 +1,46 @@
+﻿namespace UniHealth.response.API_Response
+{
+    public class APIResponse
+    {
+
+        public string Status { get; set; } 
+        public bool IsSuccess { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+        public object Data { get; set; }
+        public DateTime ResponseTime { get; } = DateTime.UtcNow;
+
+        
+        public static APIResponse Success(object data = null, string message = "تمت العملية بنجاح")
+        {
+            return new APIResponse
+            {
+                Status = "success",
+                IsSuccess = true,
+                Data = data,
+                Errors = message != null ? new List<string> { message } : new List<string>()
+            };
+        }
+
+        public static APIResponse Fail(List<string> errors, object data = null)
+        {
+            return new APIResponse
+            {
+                Status = "fail",
+                IsSuccess = false,
+                Data = data,
+                Errors = errors ?? new List<string>()
+            };
+        }
+
+        public static APIResponse Error(string error, object data = null)
+        {
+            return new APIResponse
+            {
+                Status = "error",
+                IsSuccess = false,
+                Data = data,
+                Errors = new List<string> { error }
+            };
+        }
+    }
+}
