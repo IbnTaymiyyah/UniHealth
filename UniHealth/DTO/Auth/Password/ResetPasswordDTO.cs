65fd91746957416b973353fec32fc0bd109a3634
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-16 16:03:55 +0200

    Update : Reset Password DTO

--- UniHealth/DTO/Auth/Password/ResetPasswordDTO.cs
+++ UniHealth/DTO/Auth/Password/ResetPasswordDTO.cs
@@ -5,6 +5,14 @@ namespace UniHealth.DTO.Auth.Password
     public class ResetPasswordDTO
     {
 
+        [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+        [EmailAddress(ErrorMessage = "البريد الإلكتروني غير صحيح")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "كود التحقق مطلوب")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "كود التحقق يجب أن يكون 6 أرقام")]
+        public string VerificationCode { get; set; }
+
         [Required(ErrorMessage = "كلمة المرور الجديدة مطلوبة"), DataType(DataType.Password)]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "كلمة المرور يجب أن تكون بين 6 و 100 حرف")]
         public string NewPassword { get; set; }
