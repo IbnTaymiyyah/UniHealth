d4854423e4c964216f5dd2650422b36bd5c57955
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-16 16:14:27 +0200

    Update : Auth Mapping

--- UniHealth/Mapping/AuthMappingProfile.cs
+++ UniHealth/Mapping/AuthMappingProfile.cs
@@ -11,22 +11,27 @@ namespace UniHealth.Mapping
         {
             // CreateUserDTO -> User
             CreateMap<CreateUserDTO, User>()
-                .ForMember(dest => dest.userName, opt => opt.MapFrom(src => src.Username))
-                .ForMember(dest => dest.FName, opt => opt.MapFrom(src => src.FName))
-                .ForMember(dest => dest.LName, opt => opt.MapFrom(src => src.LName))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
+                .ForMember(dest => dest.FName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.ProfileImageUrl, opt => opt.MapFrom(src => src.ProfileImageUrl))
                 .ForMember(dest => dest.imageUrl, opt => opt.Ignore());
 
             // UpdateUserDto -> User
             CreateMap<UpdateUserDto, User>()
-                .ForMember(dest => dest.FName, opt => opt.MapFrom(src => src.FName))
-                .ForMember(dest => dest.LName, opt => opt.MapFrom(src => src.LName))
+                .ForMember(dest => dest.FName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LName, opt => opt.MapFrom(src => src.LastName))
+                 .ForMember(dest => dest.ProfileImageUrl, opt => opt.MapFrom(src => src.ProfileImageUrl))
+                 .ForMember(dest => dest.imageUrl, opt => opt.MapFrom(src => src.ImageUrl))
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
 
             // User -> UserResponseDto 
             CreateMap<User, UserResponseDto>()
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FName} {src.LName}"))
-                .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.Name));
+                .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.Name))
+                .ForMember(dest => dest.ProfileImageUrl, opt => opt.MapFrom(src => src.ProfileImageUrl))
+                 .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.imageUrl));
         }
     }
 }
