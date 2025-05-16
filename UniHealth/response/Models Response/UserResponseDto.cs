aa96071fe43a0b3e6c3924b89f7d662e8525bb55
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-16 16:06:23 +0200

    Update : User Respose "Add Profile Image"

--- UniHealth/response/Models Response/UserResponseDto.cs
+++ UniHealth/response/Models Response/UserResponseDto.cs
@@ -2,12 +2,13 @@
 {
     public class UserResponseDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string RoleName { get; set; }
+        public string ProfileImageUrl { get; set; }
         public string ImageUrl { get; set; }
     }
 }
