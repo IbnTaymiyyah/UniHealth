9337789690148a97c23386094514ee8b5b046ccc
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-05-16 15:29:45 +0200

    Add : Doctor ID Model Class

---
+++ UniHealth/Models/DoctorId.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniHealth.Models
+{
+    public class DoctorId
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string DoctorUniversityId { get; set; } 
+
+        [Required]
+        public bool IsUsed { get; set; } = false; 
+
+        
+    }
+}
